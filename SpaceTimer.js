d607d0ff86b47cf0f7077b64003e423770b4a2d9
Hawkuro <hawkuro@gmail.com>
2014-11-28 16:53:42 -0800

    Works

--- SpaceTimer.js
+++ SpaceTimer.js
@@ -9,9 +9,44 @@ function timeToString(time){
 	return (time < 10?"0":"") + time + ":" + (secs < 10?"0":"") + secs + "." + (mSecs < 100?"0":"") + (mSecs < 10?"0":"") + mSecs;
 }
 
+function keyCode(keyChar) {
+	return keyChar.charCodeAt(0);
+}
+
+var Keys = (function(keys){ // Store the keys I use
+	var myKeys = {};
+	keys.forEach(function(item){
+		myKeys[item] = keyCode(item);
+	})
+	return myKeys;
+})([ // List of Keys
+	' '
+]);
+
+window.addEventListener('keyup',function(evt){
+	if(evt.keyCode === Keys[' ']){
+		finalTime = Date.now() - startTime;
+		running = false;
+	}
+});
+
+window.addEventListener('keydown',function(evt){
+	if(evt.keyCode === Keys[' ']){
+		if(running){
+			return;
+		}
+		running = true;
+		startTime = Date.now();
+	}
+});
+
 window.onload = function(){
 	function run(){
-		$("#timer").text(timeToString(finalTime++));
+		if(running){
+			$("#timer").text(timeToString(Date.now() - startTime));
+		}else{
+			$("#timer").text(timeToString(finalTime));
+		}
 		window.requestAnimationFrame(run);
 	}
 	window.requestAnimationFrame(run);
